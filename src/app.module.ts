7c33806645990c160959654f90d48a84d44380b9
German Alexis <alexisdager93@hotmail.com>
2022-09-06 17:47:17 -0500

    Project avance the pokemon api

---
+++ src/app.module.ts
@@ -0,0 +1,20 @@
+import { join } from 'path';
+import { Module } from '@nestjs/common';
+
+import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+
+import { PokemonModule } from './pokemon/pokemon.module';
+
+@Module({
+  imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'public'),
+    }),
+    MongooseModule.forRoot(
+      'mongodb+srv://admin:admin@cluster0.tletb.mongodb.net/?retryWrites=true&w=majority',
+    ),
+    PokemonModule,
+  ],
+})
+export class AppModule {}
---
+++ src/pokemon/pokemon.service.ts
@@ -0,0 +1,52 @@
+import {
+  BadRequestException,
+  Injectable,
+  InternalServerErrorException,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { CreatePokemonDto } from './dto/create-pokemon.dto';
+import { UpdatePokemonDto } from './dto/update-pokemon.dto';
+import { Pokemon } from './entities/pokemon.entity';
+
+@Injectable()
+export class PokemonService {
+  constructor(
+    @InjectModel(Pokemon.name)
+    private pokemonModel: Model<Pokemon>,
+  ) {}
+
+  async create(createPokemonDto: CreatePokemonDto) {
+    createPokemonDto.name = createPokemonDto.name.toLowerCase();
+    try {
+      const pokemon = await this.pokemonModel.create(createPokemonDto);
+      return pokemon;
+    } catch (error) {
+      if (error.code === 11000) {
+        throw new BadRequestException(
+          `Pokemon exists in DataBase ${JSON.stringify(error.keyValue)}`,
+        );
+      }
+      console.log(error);
+      throw new InternalServerErrorException(
+        `Can't create Pokemon - check server logs`,
+      );
+    }
+  }
+
+  findAll() {
+    return `This action returns all pokemon`;
+  }
+
+  findOne(id: number) {
+    return `This action returns a #${id} pokemon`;
+  }
+
+  update(id: number, updatePokemonDto: UpdatePokemonDto) {
+    return `This action updates a #${id} pokemon`;
+  }
+
+  remove(id: number) {
+    return `This action removes a #${id} pokemon`;
+  }
+}
